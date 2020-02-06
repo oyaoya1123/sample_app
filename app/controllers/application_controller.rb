60f30fc7d6ddb2422feafa61662288cc427378d0
oyaoya1123 <oyaror@yahoo.co.jp>
2020-02-06 22:56:57 +0900

    一時保存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,17 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "hello, world?"
   end
+
+  private
+  
+  # ログイン済みユーザーかどうか確認
+  # users_controller.rbから移動
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
 end
